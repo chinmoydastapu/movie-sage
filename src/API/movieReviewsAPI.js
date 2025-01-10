d0b708cb3a53150850a6f1c332cdaaa01c4b8fc5
Chinmoy Das <chinmoydastapu2@gmail.com>
2025-01-11 00:23:09 +0600

    api call updated after server deployment

--- src/API/movieReviewsAPI.js
+++ src/API/movieReviewsAPI.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:4000';
+const URL = 'https://movie-sage-backend.onrender.com';
 
 export async function readMovieReviews () {
-    // This will read the server's at http://localhost:4000/movies
+    // This will read the server's at https://movie-sage-backend.onrender.com/movies
     const response = await axios.get(`${URL}/movies`);
 
     if (response.status === 200) {
@@ -14,7 +14,7 @@ export async function readMovieReviews () {
 }
 
 export const readSingleMovieReview = async (id) => {
-    // This will read the server's at http://localhost:4000/movies/id
+    // This will read the server's at https://movie-sage-backend.onrender.com/movies/id
     const response = await axios.get(`${URL}/movies/${id}`);
 
     if (response.status === 200) {
@@ -25,28 +25,28 @@ export const readSingleMovieReview = async (id) => {
 }
 
 export const createMovieReview = async (movie) => {
-    // This will read the server's at http://localhost:4000/movies
+    // This will read the server's at https://movie-sage-backend.onrender.com/movies
     const response = await axios.post(`${URL}/movies`, movie);
 
     return response;
 }
 
 export const updateMovieReview = async (id, movie) => {
-    // This will read the server's at http://localhost:4000/movies/id
+    // This will read the server's at https://movie-sage-backend.onrender.com/movies/id
     const response = await axios.put(`${URL}/movies${id}`, movie);
 
     return response;
 }
 
 export const deleteMovieReview = async (id) => {
-    // This will read the server's at http://localhost:4000/id
+    // This will read the server's at https://movie-sage-backend.onrender.com/id
     const response = await axios.delete(`${URL}/movies/${id}`);
 
     return response;
 }
 
 export const readCarouselData = async () => {
-    // This will read the server at http://localhost:4000/carouselData
+    // This will read the server at https://movie-sage-backend.onrender.com/carouselData
     const response = await axios.get(`${URL}/carouselData`);
 
     if (response.status === 200) {
@@ -57,7 +57,7 @@ export const readCarouselData = async () => {
 }
 
 export const readMoviesByGenre = async (genres) => {
-    // This will read the server at http://localhost:4000/moviesByGenre?genres=ex1,ex2
+    // This will read the server at https://movie-sage-backend.onrender.com/moviesByGenre?genres=ex1,ex2
     try {
         const response = await axios.get(`${URL}/moviesByGenre`, {
             params: { genres: genres.join(',') }
